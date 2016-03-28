1b049e585533cada524bd07527b796ee1ca78776
Dustin <djanders@caltech.edu>
2016-03-28 10:47:07 -0700

    lhe+gen script for condor

---
+++ GridpackWorkflow/test/scripts/runLHEPythiaJob.sh
@@ -0,0 +1,87 @@
+#### !/usr/local/bin/bash
+
+### Script to generate LHE events and shower them using Pythia
+### Intended for use with Condor at UCSD
+
+### Authors:
+### Ana Ovcharova
+### Dustin Anderson
+
+PROCESS=$1
+NEVENTS=$2
+FRAGMENT=$3
+OUTDIR=$4
+RANDOM_SEED=$5
+
+CMSSW_REL="CMSSW_7_1_21_patch2"
+
+echo "Process:"
+echo $PROCESS
+
+WORKDIR=$(pwd)
+echo "Working directory: "
+echo $WORKDIR
+
+echo "Output directory set to: "
+echo $OUTDIR
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh  
+export SCRAM_ARCH=slc6_amd64_gcc481
+echo $SCRAM_ARCH
+echo $HOSTNAME
+echo "Setting up a CMSSW release..."
+eval `scramv1 project CMSSW ${CMSSW_REL}`
+cd ${CMSSW_REL}/src
+eval `scramv1 runtime -sh`
+
+echo "Prepare fragment..."
+mkdir -p Configuration/GenProduction/python
+cp ${WORKDIR}/${FRAGMENT} Configuration/GenProduction/python/genfragment.py
+
+eval "scramv1 build clean"
+eval "scramv1 build"
+
+echo "-----------> Let's begin...EVENT GENERATION"
+
+echo "Unpacking gridpack tarball..."
+tar -xaf $WORKDIR/${PROCESS}_tarball.tar.xz                                                    
+
+echo "Running event generation..."
+./runcmsgrid.sh $NEVENTS $RANDOM_SEED $(getconf _NPROCESSORS_ONLN)
+
+echo "Generation finished. Copy output..."
+LHEFILE='LHE_'${PROCESS}'_'${RANDOM_SEED}'.lhe'
+lcg-cp -v -b -D srmv2 --vo cms file:cmsgrid_final.lhe srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=${OUTDIR}/$LHEFILE
+
+echo "Fragment:"
+cat Configuration/GenProduction/python/genfragment.py
+
+#             SHOWER
+#--------------------------------
+GENFILE='GEN_'${PROCESS}'_'${FRAGMENT/.py/}'_'${RANDOM_SEED}'.root'
+
+echo "--------------> Let's begin... SHOWER"
+cmsDriver.py \
+  Configuration/GenProduction/python/genfragment.py \
+  --mc \
+  --eventcontent RAWSIM \
+  --datatier GEN-SIM \
+  --conditions auto:run2_mc \
+  --step GEN \
+  --filein file:cmsgrid_final.lhe \
+  --fileout file:${GENFILE} \
+  -n -1
+
+echo "Hadronization finished. Copy output..."
+lcg-cp -v -b -D srmv2 --vo cms file:`pwd`/${GENFILE} srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=${OUTDIR}/${GENFILE}
+
+echo "ls in cmssw src dir"
+ls
+
+#cleanup
+cd $WORKDIR
+echo "ls in workdir"
+ls
+rm -rf ${CMSSW_REL}
+
+echo "Bye."
---
+++ GridpackWorkflow/test/scripts/submitLHEPythiaCondorJob.py
@@ -0,0 +1,45 @@
+### Script to submit Condor jobs for LHE event generation and showering at UCSD
+
+### Authors:
+### Ana Ovcharova
+### Dustin Anderson
+
+import os
+import sys
+import argparse
+
+from submitLHECondorJob import submitCondorJob
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('proc', help="Names of physics model")
+    parser.add_argument('--in-file', '-i', dest='infile', help="Full path to input tarball", required=True)
+    parser.add_argument('--fragment', '-f', help='Path to gen fragment', required=True)
+    parser.add_argument('--nevents', '-n', help="Number of events per job", type=int, default=25000)
+    parser.add_argument('--njobs', '-j', help="Number of condor jobs", type=int, default=1)
+    parser.add_argument('--no-sub', dest='noSub', action='store_true', help='Do not submit jobs')
+    args = parser.parse_args()
+
+    proc = args.proc
+    infile = args.infile
+    fragment = args.fragment
+    nevents = args.nevents
+    njobs = args.njobs
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    executable = script_dir+'/runLHEPythiaJob.sh'
+    out_dir='/hadoop/cms/store/user/'+os.environ['USER']+'/mcProduction/LHERAWSIM'
+    print "Will generate LHE events using tarball",infile,"and shower them using Pythia"
+
+    #need to transfer input tarball and gen fragment
+    infiles = infile+','+fragment
+    fragfile = os.path.basename(fragment)
+
+    outdir = out_dir+'/'+proc
+    options = [proc, str(nevents), fragfile, outdir]
+    print "Options:",(' '.join(options))
+    for j in range(0,njobs):
+        rseed = str(500+j)
+        print "Random seed",rseed
+        submitCondorJob(proc, executable, options+[rseed], infiles, 
+                label=rseed+(fragfile.replace('.py','')), submit=(not args.noSub))
