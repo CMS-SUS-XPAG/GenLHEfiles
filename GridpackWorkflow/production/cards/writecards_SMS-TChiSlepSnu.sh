ea8e85f553e93d38fe55d5fcb97d2a78afc47af1
Dustin <djanders@caltech.edu>
2016-05-01 05:00:02 -0700

    modify card maker script

--- GridpackWorkflow/production/cards/writecards_SMS-TChiSlepSnu.sh
+++ GridpackWorkflow/production/cards/writecards_SMS-TChiSlepSnu.sh
@@ -3,8 +3,12 @@
 ### Use the templates to create cards for a specific mass point
 
 MNLSP=$1
+SUBDIR="jobs"
+PROC="SMS-TChiSlepSnu"
+PART="_mChi-"
+MODEL=${PROC}${PART}${MNLSP}
 
-mkdir -p SMS-TChiSlepSnu_mChi-${MNLSP}
-cp SMS-TChiSlepSnu_run_card.dat SMS-TChiSlepSnu_mChi-${MNLSP}/SMS-TChiSlepSnu_mChi-${MNLSP}_run_card.dat
-sed "s/%MNLSP%/${MNLSP}/g" SMS-TChiSlepSnu_proc_card.dat > SMS-TChiSlepSnu_mChi-${MNLSP}/SMS-TChiSlepSnu_mChi-${MNLSP}_proc_card.dat
-sed "s/%MNLSP%/${MNLSP}/g" SMS-TChiSlepSnu_customizecards.dat > SMS-TChiSlepSnu_mChi-${MNLSP}/SMS-TChiSlepSnu_mChi-${MNLSP}_customizecards.dat
+mkdir -p "${SUBDIR}/${MODEL}"
+cp ${PROC}/${PROC}_run_card.dat "${SUBDIR}/${MODEL}/${MODEL}_run_card.dat"
+sed "s/%MNLSP%/${MNLSP}/g" ${PROC}/${PROC}_proc_card.dat > "${SUBDIR}/${MODEL}/${MODEL}_proc_card.dat"
+sed "s/%MNLSP%/${MNLSP}/g" ${PROC}/${PROC}_customizecards.dat > "${SUBDIR}/${MODEL}/${MODEL}_customizecards.dat"
