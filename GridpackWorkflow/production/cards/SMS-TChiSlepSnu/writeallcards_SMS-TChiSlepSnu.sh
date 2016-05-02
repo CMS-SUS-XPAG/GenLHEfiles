6074052d0e10737fd81530c9903ca84651888434
Dustin <djanders@caltech.edu>
2016-05-02 04:47:37 -0700

    merge card scripts

--- GridpackWorkflow/production/cards/SMS-TChiSlepSnu/writeallcards_SMS-TChiSlepSnu.sh
+++ GridpackWorkflow/production/cards/SMS-TChiSlepSnu/writeallcards_SMS-TChiSlepSnu.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+JOBS="jobs"
+TEMP="templatecards"
+PROC="SMS-TChiSlepSnu"
+PART="_mChi-"
 
-### Create cards for all mass points
+### Create cards and SLHAs for all mass points
 
 for MNLSP in {100..1300..25}; do
-    source writecards_SMS-TChiSlepSnu.sh $MNLSP
+    MODEL=${PROC}${PART}${MNLSP}
+    mkdir -p "${JOBS}/${MODEL}"
+    cp ${TEMP}/${PROC}_run_card.dat "${JOBS}/${MODEL}/${MODEL}_run_card.dat"
+    sed "s/%MNLSP%/${MNLSP}/g" ${TEMP}/${PROC}_proc_card.dat > "${JOBS}/${MODEL}/${MODEL}_proc_card.dat"
+    sed "s/%MNLSP%/${MNLSP}/g" ${TEMP}/${PROC}_customizecards.dat > "${JOBS}/${MODEL}/${MODEL}_customizecards.dat"
+    sed "s/%MNLSP%/${MNLSP}/g" ${TEMP}/${PROC}.slha > ${JOBS}/${MODEL}/${MODEL}.slha
 done
--- GridpackWorkflow/production/cards/SMS-TChiSlepSnu/writeslha_SMS-TChiSlepSnu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-MODEL="SMS-TChiSlepSnu"
-JOBS="jobs"
-PART="_mChi-"
-
-for MNLSP in {100..1300..25}; do
-    sed "s/%MNLSP%/${MNLSP}/g" templatecards/${MODEL}.slha > ${JOBS}/${MODEL}${PART}${MNLSP}/${MODEL}${PART}${MNLSP}.slha
-done
