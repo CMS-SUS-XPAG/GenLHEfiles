392bca90bad81bcf0c62e437b4dcb1063239793a
Mia Liu <miaoyuan.liu0@gmail.com>
2016-05-06 03:21:47 -0700

    lhe script for stau

--- GridpackWorkflow/production/SMS-TStauStau/submitalllhejobs_SMS-TStauStau.sh
+++ GridpackWorkflow/production/SMS-TStauStau/submitalllhejobs_SMS-TStauStau.sh
@@ -2,6 +2,6 @@
 SCRIPT="../../test/scripts/submitLHECondorJob.py"
 MODEL="SMS-TStauStau_mStau-"
 
-for MNLSP in {100..1300..25}; do
-    python ${SCRIPT} ${MODEL}${MNLSP} --in-file /hadoop/cms/store/user/${USER}/mcProduction/GRIDPACKS/${MODEL}${MNLSP}/${MODEL}${MNLSP}_tarball.tar.xz
+for MNLSP in {100..450..25}; do
+    python ${SCRIPT} ${MODEL}${MNLSP} --in-file /hadoop/cms/store/user/${USER}/mcProduction/GRIDPACKS/${MODEL}${MNLSP}/${MODEL}${MNLSP}_tarball.tar.xz --proxy ${vomsdir}
 done
