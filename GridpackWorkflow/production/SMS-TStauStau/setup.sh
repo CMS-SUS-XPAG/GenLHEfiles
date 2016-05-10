209f85a90adf7803a0b8843baaa76cda1efc7a63
Mia Liu <miaoyuan.liu0@gmail.com>
2016-05-10 02:35:36 -0700

    fix proxy dir arguemtb

--- GridpackWorkflow/production/SMS-TStauStau/setup.sh
+++ GridpackWorkflow/production/SMS-TStauStau/setup.sh
@@ -1,3 +1,4 @@
+voms-proxy-init -voms cms -valid 240:00
 genprodir="/home/users/mliu/genproductions/"
 vomsdir="/tmp/x509up_u31584"
 MODEL="SMS-TStauStau_mStau-"
--- GridpackWorkflow/production/SMS-TStauStau/submitallgridpackjobs_SMS-TStauStau.sh
+++ GridpackWorkflow/production/SMS-TStauStau/submitallgridpackjobs_SMS-TStauStau.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+source setup.sh
 SCRIPT="../../test/scripts/submitGridpackCondorJob.py"
 JOBS="jobs"
+MODEL="SMS-TStauStau_mStau-"
 
 for MNLSP in {100..450..25}; do
     python ${SCRIPT} ${MODEL}${MNLSP} --cards-dir ${JOBS}/${MODEL}${MNLSP} --genproductions-dir ${genprodir} --proxy ${vomsdir}
