e507a92c18a514014bd9cc80df5df13e0283f1c2
Dustin <djanders@caltech.edu>
2016-05-11 07:21:23 -0700

    new options for pythia script

--- GridpackWorkflow/test/scripts/submitPythiaCondorJob.py
+++ GridpackWorkflow/test/scripts/submitPythiaCondorJob.py
@@ -11,27 +11,31 @@ import glob
 
 from submitLHECondorJob import submitCondorJob
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
 parser = argparse.ArgumentParser()
 parser.add_argument('proc', help="Name of physics model")
 parser.add_argument('--in-dir', '-i', dest='inDir', help="Path to input file directory", required=True)
 parser.add_argument('--slha', help="Path to slha file/fragment", required=True)
 parser.add_argument('--qcut-range', dest='qcutRange', nargs=2, type=int, default=[50,100], 
         help="Range of qcuts to scan over")
+parser.add_argument('--qcut-step', dest='qcutStep', type=int, default=2)
 parser.add_argument('--no-sub', dest='noSub', action='store_true', help='Do not submit jobs')
 parser.add_argument('--proxy', dest="proxy", help="Path to proxy", default='/tmp/x509up_u31156')
+parser.add_argument('--executable', help='Path to executable that should be run', 
+        default = script_dir+'/runPythiaJob.sh')
 args = parser.parse_args()
 
 proc = args.proc
 inDir = args.inDir
 slha = args.slha
-qcutRange = range(args.qcutRange[0], args.qcutRange[1]+1, 2)
+executable = args.executable
+qcutRange = range(args.qcutRange[0], args.qcutRange[1]+1, args.qcutStep)
 
 #get files
 inFilesList = glob.glob(inDir+'/*.lhe')
 infile = ','.join(inFilesList)+','+slha
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-executable = script_dir+'/runPythiaJob.sh'
 out_dir='/hadoop/cms/store/user/'+os.environ['USER']+'/mcProduction/RAWSIM'
 
 print "Will use Pythia to shower LHE events from files:",infile
