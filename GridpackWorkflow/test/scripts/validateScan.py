cdd09f0e834c529fb8a37917524fb2ecb1f09b12
Dustin <djanders@caltech.edu>
2016-05-25 01:21:09 -0700

    T2qq plots

--- GridpackWorkflow/test/scripts/validateScan.py
+++ GridpackWorkflow/test/scripts/validateScan.py
@@ -23,6 +23,7 @@ models["T5qqqqVV_dM20"] = model(24, 600, 1800, 26, 0, 1300, 500)
 models["T5ZZ"] = model(20, 800, 1800, 36, 0, 1800, 500)
 models["T2bb"] = model(52,300,1600,44,0,1100,500)
 models["T5Wg"] = model(52,800,2100,78,150,2100,500)
+models['T2qq'] = model(30,300,1800,70,0,1400,500)
 
 if __name__ == '__main__':
     rt.gROOT.SetBatch()
