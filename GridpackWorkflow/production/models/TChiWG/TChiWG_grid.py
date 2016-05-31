13f738edf26d659ac57c0aac05040720cf68696b
Stefano Casasso <stefano.casasso@cern.ch>
2016-05-31 03:05:34 -0700

    add TChiWG fragment and grid

---
+++ GridpackWorkflow/production/models/TChiWG/TChiWG_grid.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+### Script to define scan grid
+
+### Authors:
+### Manuel Franco Sevilla
+### Ana Ovcharova
+
+import os,sys,math
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from grid_utils import *
+
+model = "TChiWG"
+process = "C1N2"
+
+# Number of events for mass point, in thousands
+nevt = 100
+
+diag_low, diag_high = 100, 25
+xmin, xmax, xstep = 300, 1300, 25
+ymin, ymax, ystep = 300, 1300, 25
+
+# -------------------------------
+#    Constructing grid
+
+mpoints = []
+for mx in range(xmin, xmax+1, xstep):
+  my = mx
+  mpoints.append([mx,my,nevt])
+    
+## Test print out for repeated points
+mset = set()
+for mp in mpoints: mset.add(mp[0]*10000+mp[1])
+Npts, Ndiff = len(mpoints), len(mset)
+if Npts==Ndiff: print "\nGrid contains "+str(Npts)+" mass points. No duplicates\n"
+else: print "\n\nGRID CONTAINS "+str(Npts-Ndiff)+" DUPLICATE POINTS!!\n\n"
+
+# -------------------------------
+#     Plotting and printing
+
+Ntot = makePlot([mpoints], 'events', model, process, xmin, xmax, ymin, ymax)
+
+print '\nScan contains '+"{0:,.0f}".format(Ntot*1000)+" events"
+print 'Average matching efficiency (for McM and GEN fragment) = '+"{0:.3f}".format(getAveEff(mpoints,process))
+
+print
