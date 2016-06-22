d8139cfd4c2016b2a4f9e010eee946d6d6723ef0
Ana Ovcharova <akovcharova@gmail.com>
2016-06-21 17:01:25 -0700

    TChiWH_HToGG grid and fragment.

--- GridpackWorkflow/production/models/TChiWH_HtoGG/TChiWH_HtoGG_grid.py
+++ GridpackWorkflow/production/models/TChiWH_HtoGG/TChiWH_HtoGG_grid.py
@@ -10,33 +10,55 @@ import os,sys,math
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from grid_utils import *
 
-model = "TChiWH_WToLNu_HToVVTauTau"
+model = "TChiWH_HToGG"
 process = "C1N2"
 
-# Number of events for mass point, in thousands
-nevt = 100
+# Parameters that define the grid in the bulk and diagonal
+class gridBlock:
+  def __init__(self, xmin, xmax, xstep, ystep):
+    self.xmin = xmin
+    self.xmax = xmax
+    self.xstep = xstep
+    self.ystep = ystep
+    
+scanBlocks = []
+scanBlocks.append(gridBlock(150, 701, 25, 25))
 
-diag_low, diag_high = 125, 125
-xmin, xmax, xstep = 125, 700, 25
-ymin, ymax, ystep_low, ystep_high = 0, 300, 25, 25 
-
+minDM = 126
+maxDM = 176
+ymin, ymax = 0, 300
+nev = 100
+  
 # -------------------------------
 #    Constructing grid
 
+cols = []
+Nevents = []
+xmin, xmax = 9999, 0
+for block in scanBlocks:
+  Nbulk, Ndiag = 0, 0
+  for mx in range(block.xmin, block.xmax, block.xstep):
+    xmin = min(xmin, block.xmin)
+    xmax = max(xmax, block.xmax)
+    col = []
+    my = 0
+    # Adding bulk points
+    if (mx-block.xmin)%block.xstep == 0:
+      for my in range(ymin, mx-minDM, block.ystep):
+        if my > ymax: continue
+        col.append([mx,my, nev])
+        Nbulk += nev
+    if my !=  mx-minDM and mx-minDM <= ymax:
+      my = mx-minDM
+      col.append([mx,my, nev])
+      Ndiag += nev
+    cols.append(col)
+  Nevents.append([Nbulk, Ndiag])
+
 mpoints = []
-for mx in range(xmin, xmax+1, xstep):
-  ylist = []
-  if mx > (ymax + (diag_low - diag_high)): 
-    ylist.extend(range(ymin, ymax+1, ystep_low))
-  elif mx > ymax:
-    ylist.extend(range(ymin, mx - diag_low, ystep_low))
-    ylist.extend(range(mx - diag_low, ymax+1, ystep_high))
-  else:
-    ylist.extend(range(ymin, mx - diag_low, ystep_low))
-    ylist.extend(range(mx - diag_low, mx-diag_high+1, ystep_high))
-  for my in ylist:
-    if mx>my+120:
-       mpoints.append([mx,my,nevt])
+for col in cols: mpoints.extend(col)
+# add shifted point
+mpoints.append([127,0,nev])
     
 ## Test print out for repeated points
 mset = set()
