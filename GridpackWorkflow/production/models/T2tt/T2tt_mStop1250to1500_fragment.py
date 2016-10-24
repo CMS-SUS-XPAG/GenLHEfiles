24e45dee518bd5cb179d8e58366229751dd62638
Stefano Casasso <stefano.casasso@cern.ch>
2016-10-24 02:31:27 -0700

    change filenames and simplify the construction of the grid

--- GridpackWorkflow/production/models/T2tt/T2tt_mStop1250to1500_fragment.py
+++ GridpackWorkflow/production/models/T2tt/T2tt_mStop1250to1500_fragment.py
@@ -81,28 +81,7 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
     RandomizedParameters = cms.VPSet(),
 )
 
-model = "T2tt_mStop-1250to1500"
-
-# Number of events: min(goalLumi*xsec, maxEvents) (always in thousands)
-goalLumi = 400
-minLumi = 1e-40 # Skip minimum lumi
-minEvents, maxEvents = 20, 1000
-diagStep, bandStep = 25, 50
-midDM, maxDM = 300, 700
-addDiag = [183, 167] # DeltaM for additional diagonal lines to be added
-
-# Parameters that define the grid in the bulk and diagonal
-class gridBlock:
-  def __init__(self, xmin, xmax, xstep, ystep):
-    self.xmin = xmin
-    self.xmax = xmax
-    self.xstep = xstep
-    self.ystep = ystep
-
-scanBlocks = []
-scanBlocks.append(gridBlock(1250,  1501, 50, 50))
-minDM = 85
-ymin, ymed, ymax = 0, 0, 650 
+model = "T2tt_mStop1250to1500"
 
 
 def matchParams(mass):
@@ -114,92 +93,27 @@ def matchParams(mass):
   elif mass<1299: return 68., 0.237
   elif mass<1451: return 70., 0.243
   elif mass<1801: return 74., 0.246
-
+  
 
 def xsec(mass):
   if mass < 300: return 319925471928717.38*math.pow(mass, -4.10396285974583*math.exp(mass*0.0001317804474363))
   else: return 6953884830281245*math.pow(mass, -4.7171617288678069*math.exp(mass*6.1752771466190749e-05))
 
 # Number of events for mass point, in thousands
-def events(mass):
-  xs = xsec(mass)
-  nev = min(goalLumi*xs, maxEvents*1000)
-  if nev < xs*minLumi: nev = xs*minLumi
-  nev = max(nev/1000, minEvents)
-  return math.ceil(nev) # Rounds up
+nevt = 20
+
+xmin, xmax, xstep = 1250, 1500, 50
+ymin, ymax, ystep = 0, 650, 50
 
 # -------------------------------
 #    Constructing grid
 
-cols = []
-Nevents = []
-xmin, xmax = 9999, 0
-for block in scanBlocks:
-  Nbulk, Ndiag = 0, 0
-  for mx in range(block.xmin, block.xmax, min(bandStep, diagStep)):
-    xmin = min(xmin, block.xmin)
-    xmax = max(xmax, block.xmax)
-    col = []
-    my = 0
-    begBand = min(max(ymed, mx-maxDM), mx-minDM)
-    begDiag = min(max(ymed, mx-midDM), mx-minDM)
-    # Adding bulk points
-    if (mx-block.xmin)%block.xstep == 0 :
-      for my in range(ymin, begBand, block.ystep):
-        if my > ymax: continue
-        # Adding extra diagonals to the bulk
-        for dm in addDiag:
-          dm_before = mx-block.xstep -my
-          dm_after = mx - my
-          if(dm>dm_before and dm<dm_after):
-            nev = events(my+dm)
-            col.append([my+dm,my, nev])
-            Nbulk += nev
-        nev = events(mx)
-        col.append([mx,my, nev])
-        Nbulk += nev
-    # Adding diagonal points in inside band
-    if (mx-block.xmin)%bandStep == 0 :
-      for my in range(begBand, mx-midDM, bandStep):
-        if my > ymax: continue
-        # Adding extra diagonals to the band
-        for dm in addDiag:
-          dm_before = mx-bandStep -my
-          dm_after = mx - my
-          if(dm>dm_before and dm<dm_after):
-            nev = events(my+dm)
-            col.append([my+dm,my, nev])
-            Ndiag += nev
-        # Adding standard diagonal points
-        nev = events(mx)
-        col.append([mx,my, nev])
-        Ndiag += nev
-    # Adding diagonal points in band closest to outer diagonal
-    for my in range(begDiag, mx-minDM+1, diagStep):
-      if my > ymax: continue
-      # Adding extra diagonals to the band
-      for dm in addDiag:
-        dm_before = mx-diagStep -my
-        dm_after = mx - my
-        if(dm>dm_before and dm<dm_after):
-          nev = events(my+dm)
-          col.append([my+dm,my, nev])
-          Ndiag += nev
-      nev = events(mx)
-      col.append([mx,my, nev])
-      Ndiag += nev
-    if(my !=  mx-minDM and mx-minDM <= ymax):
-      my = mx-minDM
-      nev = events(mx)
-      col.append([mx,my, nev])
-      Ndiag += nev
-    cols.append(col)
-  Nevents.append([Nbulk, Ndiag])
-
 mpoints = []
-for col in cols: 
-  for ipt in col:
-    if (ipt[0]<251): mpoints.append(ipt)
+for mx in range(xmin, xmax+1, xstep):
+  for my in range(ymin, ymax+1, ystep):
+      mpoints.append([mx,my,nevt])
+
+
 
 for point in mpoints:
     mstop, mlsp = point[0], point[1]
