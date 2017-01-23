d56fd04e7cd040714534cf00a6212837ddcdab06
Mia Liu <miaoyuan.liu0@gmail.com>
2017-01-23 05:01:19 -0800

    add T5qqqqZH

---
+++ GridpackWorkflow/production/models/T5qqqqZH/T5qqqqZH_fragment.py
@@ -0,0 +1,159 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+import math
+
+mglu = 700
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args =  cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.3/sus_sms/SMS-GlGl/SMS-GlGl_mGl-%i_tarball.tar.xz' % mglu),
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+baseSLHATable="""
+BLOCK MASS  # Mass Spectrum
+# PDG code           mass       particle
+   1000001     1.00000000E+05   # ~d_L
+   2000001     1.00000000E+05   # ~d_R
+   1000002     1.00000000E+05   # ~u_L
+   2000002     1.00000000E+05   # ~u_R
+   1000003     1.00000000E+05   # ~s_L
+   2000003     1.00000000E+05   # ~s_R
+   1000004     1.00000000E+05   # ~c_L
+   2000004     1.00000000E+05   # ~c_R
+   1000005     1.00000000E+05   # ~b_1
+   2000005     1.00000000E+05   # ~b_2
+   1000006     1.00000000E+05   # ~t_1
+   2000006     1.00000000E+05   # ~t_2
+   1000011     1.00000000E+05   # ~e_L
+   2000011     1.00000000E+05   # ~e_R
+   1000012     1.00000000E+05   # ~nu_eL
+   1000013     1.00000000E+05   # ~mu_L
+   2000013     1.00000000E+05   # ~mu_R
+   1000014     1.00000000E+05   # ~nu_muL
+   1000015     1.00000000E+05   # ~tau_1
+   2000015     1.00000000E+05   # ~tau_2
+   1000016     1.00000000E+05   # ~nu_tauL
+   1000021     %MGLU%           # ~g
+   1000022     %MLSP%           # ~chi_10
+   1000023     %MNLSP%          # ~chi_20
+   1000025     1.00000000E+05   # ~chi_30
+   1000035     1.00000000E+05   # ~chi_40
+   1000024     1.00000000E+05   # ~chi_1+
+   1000037     1.00000000E+05   # ~chi_2+
+
+# DECAY TABLE
+#         PDG            Width
+DECAY   1000001     0.00000000E+00   # sdown_L decays
+DECAY   2000001     0.00000000E+00   # sdown_R decays
+DECAY   1000002     0.00000000E+00   # sup_L decays
+DECAY   2000002     0.00000000E+00   # sup_R decays
+DECAY   1000003     0.00000000E+00   # sstrange_L decays
+DECAY   2000003     0.00000000E+00   # sstrange_R decays
+DECAY   1000004     0.00000000E+00   # scharm_L decays
+DECAY   2000004     0.00000000E+00   # scharm_R decays
+DECAY   1000005     0.00000000E+00   # sbottom1 decays
+DECAY   2000005     0.00000000E+00   # sbottom2 decays
+DECAY   1000006     0.00000000E+00   # stop1 decays
+DECAY   2000006     0.00000000E+00   # stop2 decays
+
+DECAY   1000011     0.00000000E+00   # selectron_L decays
+DECAY   2000011     0.00000000E+00   # selectron_R decays
+DECAY   1000012     0.00000000E+00   # snu_elL decays
+DECAY   1000013     0.00000000E+00   # smuon_L decays
+DECAY   2000013     0.00000000E+00   # smuon_R decays
+DECAY   1000014     0.00000000E+00   # snu_muL decays
+DECAY   1000015     0.00000000E+00   # stau_1 decays
+DECAY   2000015     0.00000000E+00   # stau_2 decays
+DECAY   1000016     0.00000000E+00   # snu_tauL decays
+DECAY   1000021     1.00000000E+00   # gluino decays
+#           BR         NDA      ID1       ID2       ID3
+0.25E+00    3     1000023        -1        1   #BR(~gl -> N2 q qbar)
+0.25E+00    3     1000023        -2        2   #BR(~gl -> N2 q qbar)
+0.25E+00    3     1000023        -3        3   # BR(~gl -> N2 q qbar)
+0.25E+00    3     1000023        -4        4   # BR(~gl -> N2 q qbar)
+DECAY   1000022     0.00000000E+00   # neutralino1 decays
+DECAY   1000023     0.10000000E+00   # neutralino2 decays
+#           BR         NDA      ID1       ID2 
+0.00000000E+00    3     1000022        -1      1     # Dummy decay to allow off-shell Z
+0.500000000E+00   2     1000022        23            # BR(N2 -> N1 + Z)
+0.500000000E+00   2     1000022        25            # BR(N2 -> N1 + Z)
+DECAY   1000024     0.00000000E+00   # chargino1+ decays
+DECAY   1000025     0.00000000E+00   # neutralino3 decays
+DECAY   1000035     0.00000000E+00   # neutralino4 decays
+DECAY   1000037     0.00000000E+00   # chargino2+ decays
+"""
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    RandomizedParameters = cms.VPSet(),
+)
+
+model = "T5qqqqZH"
+# weighted average of matching efficiencies for the full scan
+# must equal the number entered in McM generator params
+mcm_eff = 0.250
+
+# Fit to gluino cross-section in fb
+def xsec(mass):
+    return 4.563e+17*math.pow(mass, -4.761*math.exp(5.848e-05*mass))
+
+def matchParams(mass):
+    if   mass<799: return 118., 0.235
+    elif mass<999: return 128., 0.235
+    elif mass<1199: return 140., 0.235
+    elif mass<1399: return 143., 0.245
+    elif mass<1499: return 147., 0.255
+    elif mass<1799: return 150., 0.267
+    elif mass<2099: return 156., 0.290
+    else: return 160., 0.315
+
+# -------------------------------
+mnlsp = mglu - 50
+qcut, tru_eff = matchParams(mglu)
+#wgt = point[2]*(mcm_eff/tru_eff)
+
+mlsp = 1
+slhatable = baseSLHATable.replace('%MGLU%','%e' % mglu)
+slhatable = slhatable.replace('%MLSP%','%e' % mlsp)
+slhatable = slhatable.replace('%MNLSP%','%e' % mnlsp)
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+  maxEventsToPrint = cms.untracked.int32(1),
+  pythiaPylistVerbosity = cms.untracked.int32(1),
+  filterEfficiency = cms.untracked.double(1.0),
+  pythiaHepMCVerbosity = cms.untracked.bool(False),
+  comEnergy = cms.double(13000.),
+  PythiaParameters = cms.PSet(
+    pythia8CommonSettingsBlock,
+    pythia8CUEP8M1SettingsBlock,
+    JetMatchingParameters = cms.vstring(
+      'JetMatching:setMad = off',
+      'JetMatching:scheme = 1',
+      'JetMatching:merge = on',
+      'JetMatching:jetAlgorithm = 2',
+      'JetMatching:etaJetMax = 5.',
+      'JetMatching:coneRadius = 1.',
+      'JetMatching:slowJetPower = 1',
+      'JetMatching:qCut = %e' % qcut, #this is the actual merging scale
+      'JetMatching:nQmatch = 5', #4 corresponds to 4-flavour scheme (no matching of b-quarks), 5 for 5-flavour scheme
+      'JetMatching:nJetMax = 2', #number of partons in born matrix element for highest multiplicity
+      'JetMatching:doShowerKt = off', #off for MLM matching, turn on for shower-kT matching
+      '25:m0 = 125.0',
+    ), 
+    parameterSets = cms.vstring('pythia8CommonSettings',
+      'pythia8CUEP8M1Settings',
+      'JetMatchingParameters'
+    )
+  ),
+  SLHATableForPythia8 = cms.string(slhatable),
+)
