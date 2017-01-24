a41e6d398ed82ccde290e02aeaf22f610246a53f
Stefano Casasso <stefano.casasso@cern.ch>
2017-01-24 01:42:36 -0800

    modify proc_card for tHW and keep only gridpack scripts

---
+++ GridpackWorkflow/production/tHW/copyGridpacksToEOS_tHW.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+GRIDPACKDIR=/hadoop/cms/store/user/${USER}/mcProduction/GRIDPACKS
+MODEL='tHW_mH-'
+OUTMODEL='tHW'
+PART='_mH-'
+
+for MASS in {350..550..20}; do  
+    echo "Copying ${GRIDPACKDIR}/${MODEL}${MASS}/${MODEL}${MASS}_tarball.tar.xz to the following directory on EOS:"
+    echo "/store/group/phys_generator/cvmfs/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.3/sus_sms/${OUTMODEL}/${OUTMODEL}${PART}${MASS}_tarball.tar.xz"
+    xrdcp ${GRIDPACKDIR}/${MODEL}${MASS}/${MODEL}${MASS}_tarball.tar.xz root://eoscms.cern.ch///eos/cms/store/group/phys_generator/cvmfs/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.3/sus_sms/${OUTMODEL}/${OUTMODEL}${PART}${MASS}_tarball.tar.xz
+done
--- GridpackWorkflow/production/tHW/submitallpythiajobs_tHW.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SCRIPT="../../test/scripts/submitPythiaCondorJob.py"
-MODEL="tHW"
-JOBS="jobs"
-NJETMAX=1
-
-for MPROD in {350..550..20}; do
-    python ${SCRIPT} ${MODEL}_mH-${MPROD} --in-dir /hadoop/cms/store/user/${USER}/mcProduction/LHE/${MODEL}_mH-${MPROD} --slha ${JOBS}/${MODEL}_mH-${MPROD}/${MODEL}_mH-${MPROD}.slha --qcut-range 60 90 --proxy ${vomsdir} --nJetMax 1
-done
