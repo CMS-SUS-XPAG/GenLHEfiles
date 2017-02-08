089fe62d099cc86e1f7c3c19bc959f3566930c8a
Stefano Casasso <stefano.casasso@cern.ch>
2017-02-08 00:40:16 -0800

    small fixes to SqSqGamma gridpack scripts

--- GridpackWorkflow/production/SMS-SqSqPlusGamma/makealldjrplots_SMS-SqSqPlusGamma.sh
+++ GridpackWorkflow/production/SMS-SqSqPlusGamma/makealldjrplots_SMS-SqSqPlusGamma.sh
@@ -5,5 +5,5 @@ SCRIPT="../../test/scripts/djr.py"
 MODEL="SMS-SqSqPlusGamma_mSq-"
 
 for MPROD in {200..500..50}; do
-    python ${SCRIPT} ${MODEL}${MPROD} /hadoop/cms/store/user/${USER}/mcProduction/RAWSIM/${MODEL}${MPROD} --qcut-range 55 59
+    python ${SCRIPT} ${MODEL}${MPROD} /hadoop/cms/store/user/${USER}/mcProduction/RAWSIM/${MODEL}${MPROD} --qcut-range 50 70
 done
--- GridpackWorkflow/production/SMS-SqSqPlusGamma/submitallpythiajobs_SMS-SqSqPlusGamma.sh
+++ GridpackWorkflow/production/SMS-SqSqPlusGamma/submitallpythiajobs_SMS-SqSqPlusGamma.sh
@@ -4,6 +4,6 @@ MODEL="SMS-SqSqPlusGamma_mSq-"
 JOBS="jobs"
 
 for MPROD in {200..500..50}; do
-    python ${SCRIPT} ${MODEL}${MPROD} --in-dir /hadoop/cms/store/user/${USER}/mcProduction/LHE/${MODEL}${MPROD} --slha ${JOBS}/${MODEL}${MPROD}/${MODEL}${MPROD}.slha --qcut-range 50 80 --proxy ${vomsdir}
+    python ${SCRIPT} ${MODEL}${MPROD} --in-dir /hadoop/cms/store/user/${USER}/mcProduction/LHE/${MODEL}${MPROD} --slha ${JOBS}/${MODEL}${MPROD}/${MODEL}${MPROD}.slha --qcut-range 50 70 --proxy ${vomsdir} --nJetMax 1
 done
 
