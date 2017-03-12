707bd9f3bc95533d57852b93d37cf0fc312510b8
Lesya Shchutska <lesya.shchutska@cern.ch>
2017-03-12 03:38:04 -0700

    StauStau: extended gridpack range to 1000

--- GridpackWorkflow/production/SMS-TStauStau/makealldjrplots_SMS-TStauStau.sh
+++ GridpackWorkflow/production/SMS-TStauStau/makealldjrplots_SMS-TStauStau.sh
@@ -4,6 +4,6 @@ QMAX=$2
 SCRIPT="../../test/scripts/djr.py"
 MODEL="SMS-TStauStau_mStau-"
 
-for MNLSP in {100..1300..100}; do
+for MNLSP in {100..1000..100}; do
     python ${SCRIPT} ${MODEL}${MNLSP} /hadoop/cms/store/user/${USER}/mcProduction/RAWSIM/${MODEL}${MNLSP} --qcut-range ${QMIN} ${QMAX}
 done
--- GridpackWorkflow/production/SMS-TStauStau/submitallgridpackjobs_SMS-TStauStau.sh
+++ GridpackWorkflow/production/SMS-TStauStau/submitallgridpackjobs_SMS-TStauStau.sh
@@ -4,6 +4,6 @@ SCRIPT="../../test/scripts/submitGridpackCondorJob.py"
 JOBS="jobs"
 MODEL="SMS-TStauStau_mStau-"
 
-for MNLSP in {100..450..25}; do
+for MNLSP in {100..1000..25}; do
     python ${SCRIPT} ${MODEL}${MNLSP} --cards-dir ${JOBS}/${MODEL}${MNLSP} --genproductions-dir ${genprodir} --proxy ${vomsdir}
 done
