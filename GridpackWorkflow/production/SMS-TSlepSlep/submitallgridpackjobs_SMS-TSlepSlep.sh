a01c5110430b5da70fde2d2f57a9a43fc8ab610d
Lesya Shchutska <lesya.shchutska@cern.ch>
2017-03-12 03:33:59 -0700

    SlepSlep: extended gridpack range to 1000

--- GridpackWorkflow/production/SMS-TSlepSlep/submitallgridpackjobs_SMS-TSlepSlep.sh
+++ GridpackWorkflow/production/SMS-TSlepSlep/submitallgridpackjobs_SMS-TSlepSlep.sh
@@ -4,7 +4,7 @@ SCRIPT="../../test/scripts/submitGridpackCondorJob.py"
 JOBS="jobs"
 MODEL="SMS-TSlepSlep_mSlep-"
 
-for MNLSP in {100..450..25}; do
+for MNLSP in {100..1000..25}; do
 #    if [ $MNLSP -eq 100 ]; then
     python ${SCRIPT} ${MODEL}${MNLSP} --cards-dir ${JOBS}/${MODEL}${MNLSP} --genproductions-dir ${genprodir} --proxy ${vomsdir}
 #    fi
--- GridpackWorkflow/production/SMS-TSlepSlep/submitalllhejobs_SMS-TSlepSlep.sh
+++ GridpackWorkflow/production/SMS-TSlepSlep/submitalllhejobs_SMS-TSlepSlep.sh
@@ -3,6 +3,6 @@ SCRIPT="../../test/scripts/submitLHECondorJob.py"
 MODEL="SMS-TSlepSlep_mSlep-"
 vomsdir="/tmp/x509up_u31606"
 
-for MNLSP in {100..450..25}; do
+for MNLSP in {100..1000..25}; do
     python ${SCRIPT} ${MODEL}${MNLSP} --in-file /hadoop/cms/store/user/${USER}/mcProduction/GRIDPACKS/${MODEL}${MNLSP}/${MODEL}${MNLSP}_tarball.tar.xz --proxy ${vomsdir}
 done
