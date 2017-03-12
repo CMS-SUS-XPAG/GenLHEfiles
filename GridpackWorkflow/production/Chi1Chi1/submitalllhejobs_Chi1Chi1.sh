e4fae2c89483c7b99a626bf0de193af6e33123d2
Lesya Shchutska <lesya.shchutska@cern.ch>
2017-03-12 03:19:57 -0700

    C1C1: extended gridpack range to 1500

--- GridpackWorkflow/production/Chi1Chi1/submitalllhejobs_Chi1Chi1.sh
+++ GridpackWorkflow/production/Chi1Chi1/submitalllhejobs_Chi1Chi1.sh
@@ -2,6 +2,6 @@
 SCRIPT="../../test/scripts/submitLHECondorJob.py"
 MODEL="Chi1Chi1_mChi-"
 
-for MNLSP in {100..800..25}; do
+for MNLSP in {100..1500..25}; do
     python ${SCRIPT} ${MODEL}${MNLSP} --in-file /hadoop/cms/store/user/${USER}/mcProduction/GRIDPACKS/${MODEL}${MNLSP}/${MODEL}${MNLSP}_tarball.tar.xz
 done
--- GridpackWorkflow/production/Chi1Chi1/submitallpythiajobs_Chi1Chi1.sh
+++ GridpackWorkflow/production/Chi1Chi1/submitallpythiajobs_Chi1Chi1.sh
@@ -5,6 +5,6 @@ SCRIPT="../../test/scripts/submitPythiaCondorJob.py"
 MODEL="Chi1Chi1_mChi-"
 JOBS="jobs"
 
-for MNLSP in {100..800..50}; do
+for MNLSP in {100..1500..50}; do
     python ${SCRIPT} ${MODEL}${MNLSP} --in-dir /hadoop/cms/store/user/${USER}/mcProduction/LHE/${MODEL}${MNLSP} --slha ${JOBS}/${MODEL}${MNLSP}/${MODEL}${MNLSP}.slha --qcut-range ${QMIN} ${QMAX}
 done
