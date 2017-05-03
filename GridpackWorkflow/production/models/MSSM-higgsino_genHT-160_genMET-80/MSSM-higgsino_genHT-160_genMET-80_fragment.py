7d6c628d8d851d47af851618cedb065d81d8da2e
Lesya Shchutska <lesya.shchutska@cern.ch>
2017-05-03 04:14:58 -0700

    MSSM-higgsino: test points for 0L and 1L

---
+++ GridpackWorkflow/production/models/MSSM-higgsino_genHT-160_genMET-80/MSSM-higgsino_genHT-160_genMET-80_fragment.py
@@ -0,0 +1,195 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+import math
+
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    RandomizedParameters = cms.VPSet(),
+)
+
+def matchParams(mass):
+    if mass < 110.: return 76,0.645
+    elif mass < 130.: return 76,0.617
+    elif mass < 150.: return 76,0.591
+    elif mass < 170.: return 76,0.581
+    elif mass < 190.: return 76,0.562
+    elif mass < 210.: return 76,0.543
+    else: return 76,0.529
+
+model = "MSSM-higgsino"
+process = "Higgsino_Full"
+
+# Number of events for mass point, in thousands
+nevt = 500
+# -------------------------------
+#    Constructing grid
+
+mpoints = []
+mpoints.append([220, 300, nevt])
+mpoints.append([160, 500, nevt])
+mpoints.append([120, 1000, nevt])
+
+for point in mpoints:
+
+    mu, m1 = point[0], point[1]
+
+    muStr = str(int(mu))
+    m1Str = str(int(m1))
+
+    qcut, tru_eff = matchParams(mu)
+    wgt = point[2]/tru_eff
+  
+    slhaTable = '' 
+    with open('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.3/sus_sms/slha/Higgsino_Full/susyhit_slha_%d_%d.out' % (mu, m1)) as f:
+        slhatable = f.read()
+
+    basePythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        JetMatchingParameters = cms.vstring(
+            'JetMatching:setMad = off',
+            'JetMatching:scheme = 1',
+            'JetMatching:merge = on',
+            'JetMatching:jetAlgorithm = 2',
+            'JetMatching:etaJetMax = 5.',
+            'JetMatching:coneRadius = 1.',
+            'JetMatching:slowJetPower = 1',
+            'JetMatching:qCut = %.0f' % qcut, #this is the actual merging scale
+            'JetMatching:nQmatch = 5', #4 corresponds to 4-flavour scheme (no matching of b-quarks), 5 for 5-flavour scheme
+            'JetMatching:nJetMax = 2', #number of partons in born matrix element for highest multiplicity
+            'JetMatching:doShowerKt = off', #off for MLM matching, turn on for shower-kT matching
+            '6:m0 = 172.5',
+            '24:mMin = 0.1',
+            '23:mMin = 0.1',
+            'Check:abortIfVeto = on',
+        ), 
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'JetMatchingParameters'
+        )
+    )
+    generator.RandomizedParameters.append(
+        cms.PSet(
+            ConfigWeight = cms.double(wgt),
+            GridpackPath =  cms.string('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.3/sus_sms/%s/%s_MU-%s_M1-%s_tarball.tar.xz' % (process,process,muStr,m1Str)),
+            ConfigDescription = cms.string('%s_%s_%s' % (model, muStr,m1Str)),
+            SLHATableForPythia8 = cms.string('%s' % slhatable),
+            PythiaParameters = basePythiaParameters,
+        ),
+    )
+
+
+
+#     Filter setup
+# ------------------------
+# https://github.com/cms-sw/cmssw/blob/CMSSW_8_0_X/PhysicsTools/HepMCCandAlgos/python/genParticles_cfi.py
+tmpGenParticles = cms.EDProducer("GenParticleProducer",
+    saveBarCodes = cms.untracked.bool(True),
+    src = cms.InputTag("generator","unsmeared"),
+    abortOnUnknownPDGCode = cms.untracked.bool(False)
+)
+
+# https://github.com/cms-sw/cmssw/blob/CMSSW_8_0_X/RecoJets/Configuration/python/GenJetParticles_cff.py
+# https://github.com/cms-sw/cmssw/blob/CMSSW_8_0_X/RecoMET/Configuration/python/GenMETParticles_cff.py
+tmpGenParticlesForJetsNoNu = cms.EDProducer("InputGenJetsParticleSelector",
+    src = cms.InputTag("tmpGenParticles"),
+    ignoreParticleIDs = cms.vuint32(
+         1000022, 1000023, 1000024,
+         1000012, 1000014, 1000016,
+         2000012, 2000014, 2000016,
+         1000039, 5100039,
+         4000012, 4000014, 4000016,
+         9900012, 9900014, 9900016,
+         39,12,14,16),
+    partonicFinalState = cms.bool(False),
+    excludeResonances = cms.bool(False),
+    excludeFromResonancePids = cms.vuint32(12, 13, 14, 16),
+    tausAsJets = cms.bool(False)
+)
+
+# https://github.com/cms-sw/cmssw/blob/CMSSW_8_0_X/RecoJets/JetProducers/python/AnomalousCellParameters_cfi.py
+AnomalousCellParameters = cms.PSet(
+    maxBadEcalCells         = cms.uint32(9999999),
+    maxRecoveredEcalCells   = cms.uint32(9999999),
+    maxProblematicEcalCells = cms.uint32(9999999),
+    maxBadHcalCells         = cms.uint32(9999999),
+    maxRecoveredHcalCells   = cms.uint32(9999999),
+    maxProblematicHcalCells = cms.uint32(9999999)
+)
+
+# https://github.com/cms-sw/cmssw/blob/CMSSW_8_0_X/RecoJets/JetProducers/python/GenJetParameters_cfi.py
+GenJetParameters = cms.PSet(
+    src            = cms.InputTag("tmpGenParticlesForJetsNoNu"),
+    srcPVs         = cms.InputTag(''),
+    jetType        = cms.string('GenJet'),
+    jetPtMin       = cms.double(3.0),
+    inputEtMin     = cms.double(0.0),
+    inputEMin      = cms.double(0.0),
+    doPVCorrection = cms.bool(False),
+    # pileup with offset correction
+    doPUOffsetCorr = cms.bool(False),
+       # if pileup is false, these are not read:
+       nSigmaPU = cms.double(1.0),
+       radiusPU = cms.double(0.5),
+    # fastjet-style pileup
+    doAreaFastjet  = cms.bool(False),
+    doRhoFastjet   = cms.bool(False),
+      # if doPU is false, these are not read:
+      Active_Area_Repeats = cms.int32(5),
+      GhostArea = cms.double(0.01),
+      Ghost_EtaMax = cms.double(6.0),
+    Rho_EtaMax = cms.double(4.5),
+    useDeterministicSeed= cms.bool( True ),
+    minSeed             = cms.uint32( 14327 )
+)
+
+
+# https://github.com/cms-sw/cmssw/blob/CMSSW_8_0_X/RecoJets/JetProducers/python/ak4GenJets_cfi.py
+tmpAk4GenJetsNoNu = cms.EDProducer(
+    "FastjetJetProducer",
+    GenJetParameters,
+    AnomalousCellParameters,
+    jetAlgorithm = cms.string("AntiKt"),
+    rParam       = cms.double(0.4)
+)
+
+genHTFilter = cms.EDFilter("GenHTFilter",
+   src = cms.InputTag("tmpAk4GenJetsNoNu"), #GenJet collection as input
+   jetPtCut = cms.double(30.0), #GenJet pT cut for HT
+   jetEtaCut = cms.double(5.0), #GenJet eta cut for HT
+   genHTcut = cms.double(160.0) #genHT cut
+)
+
+
+tmpGenMetTrue = cms.EDProducer("GenMETProducer",
+    src = cms.InputTag("tmpGenParticlesForJetsNoNu"),
+    onlyFiducialParticles = cms.bool(False), ## Use only fiducial GenParticles
+    globalThreshold = cms.double(0.0), ## Global Threshold for input objects
+    usePt   = cms.bool(True), ## using Pt instead Et
+    applyFiducialThresholdForFractions   = cms.bool(False),
+)
+
+genMETfilter1 = cms.EDFilter("CandViewSelector",
+   src = cms.InputTag("tmpGenMetTrue"),
+   cut = cms.string("pt > 80")
+)
+
+genMETfilter2 = cms.EDFilter("CandViewCountFilter",
+    src = cms.InputTag("genMETfilter1"),
+    minNumber = cms.uint32(1),
+)
+
+
+ProductionFilterSequence = cms.Sequence(generator*
+                                        tmpGenParticles * tmpGenParticlesForJetsNoNu *
+                                        tmpAk4GenJetsNoNu * genHTFilter *
+                                        tmpGenMetTrue * genMETfilter1 * genMETfilter2
+)
