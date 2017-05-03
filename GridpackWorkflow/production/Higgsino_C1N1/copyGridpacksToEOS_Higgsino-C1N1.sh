8670b1e6297a306f86dc55f9bbc90ef30b98ad20
Lesya Shchutska <lesya.shchutska@cern.ch>
2017-05-03 09:46:20 -0700

    C1N1: gridpack copy to eos

--- GridpackWorkflow/production/Higgsino_C1N1/copyGridpacksToEOS_Higgsino-C1N1.sh
+++ GridpackWorkflow/production/Higgsino_C1N1/copyGridpacksToEOS_Higgsino-C1N1.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 GRIDPACKDIR=/hadoop/cms/store/user/${USER}/mcProduction/GRIDPACKS
-MODEL='Higgsino-N2C1'
-PARTN2='_mN2-'
+MODEL='Higgsino-C1N1'
+PARTN1='_mN1-'
 PARTC1='_mC1-'
 
-for MN2 in 100 120 140 160 180 200 220 240; do
-    for DM in 7.5 10 15 20 30 40; do
-	MN1=`awk "BEGIN {printf \"%.2f\n\", (${MN2}-${DM})}"`	
-	MC1=`awk "BEGIN {printf \"%.2f\n\", ((${MN1}+${MN2})/2)}"`
-	MN2STR=${MN2/./p}
+for MC1 in 80 100 120 140 160 180 200 220 240; do
+    for DM in 3 7.5 10 15 20 30 40; do
+	MN1=`awk "BEGIN {printf \"%.2f\n\", (${MC1}-${DM})}"`	
 	MN1STR=${MN1/./p}
 	MC1STR=${MC1/./p}
-	MODELSTR=${MODEL}${PARTN2}${MN2STR}${PARTC1}${MC1STR}	
+	MODELSTR=${MODEL}${PARTC1}${MC1STR}${PARTN1}${MN1STR}	
 	echo "Copying ${GRIDPACKDIR}/${MODELSTR}/${MODELSTR}_tarball.tar.xz to the following directory on EOS:"
 	echo "/store/group/phys_generator/cvmfs/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.3/sus_sms/${MODEL}/${MODELSTR}_tarball.tar.xz"
 	xrdcp ${GRIDPACKDIR}/${MODELSTR}/${MODELSTR}_tarball.tar.xz root://eoscms.cern.ch///eos/cms/store/group/phys_generator/cvmfs/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.3/sus_sms/${MODEL}/${MODELSTR}_tarball.tar.xz
