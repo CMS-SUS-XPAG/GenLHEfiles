09b09aaa5ee4096befc72532191c77026c38f54e
vdutta <vdutta@mit.edu>
2017-05-30 13:09:40 -0700

    slepton fragment

---
+++ GridpackWorkflow/production/models/TSlepSlep/TSlepSlep_fragment.py
@@ -0,0 +1,159 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+import math
+baseSLHATable="""
+BLOCK MASS  # Mass Spectrum
+# PDG code           mass       particle
+        35     1.00000000E+05
+        36     1.00000000E+05
+        37     1.00000000E+05
+        6      1.72500000E+02
+   1000001     1.00000000E+05    # ~d_L
+   2000001     1.00000000E+05   # ~d_R
+   1000002     1.00000000E+05    # ~u_L
+   2000002     1.00000000E+05   # ~u_R
+   1000003     1.00000000E+05    # ~s_L
+   2000003     1.00000000E+05   # ~s_R
+   1000004     1.00000000E+05    # ~c_L
+   2000004     1.00000000E+05   # ~c_R
+   1000005     1.00000000E+05   # ~b_1
+   2000005     1.00000000E+05   # ~b_2
+   1000006     1.00000000E+05   # ~t_1
+   2000006     1.00000000E+05   # ~t_2
+   1000011     %MSLEP%          # ~e_L
+   2000011     %MSLEP%          # ~e_R
+   1000012     1.00000000E+05   # ~nu_eL
+   1000013     %MSLEP%          # ~mu_L
+   2000013     %MSLEP%          # ~mu_R
+   1000014     1.00000000E+05   # ~nu_muL
+   1000015     1.00000000E+05   # ~tau_1
+   2000015     1.00000000E+05   # ~tau_2
+   1000016     1.00000000E+05   # ~nu_tauL
+   1000021     1.00000000E+05   # ~g
+   1000022     %MLSP%           # ~chi_10
+   1000023     1.00000000E+05   # ~chi_20
+   1000025     1.00000000E+05   # ~chi_30
+   1000035     1.00000000E+05   # ~chi_40
+   1000024     1.00000000E+05   # ~chi_1+
+   1000037     1.00000000E+05   # ~chi_2+
+#
+#
+#
+#         PDG            Width
+DECAY         6     1.134E+00        # top decays
+DECAY   2000006     0.00000000E+00   # stop2 decays
+DECAY   1000005     0.00000000E+00   # sbottom1 decays
+DECAY   2000005     0.00000000E+00   # sbottom2 decays
+#
+#         PDG            Width
+DECAY   1000011     0.10000000E+00   # selectron_L decays
+    1.00000000E+00    2    1000022    11
+DECAY   2000011     0.10000000E+00   # selectron_R decays
+    1.00000000E+00    2    1000022    11
+DECAY   1000013     0.10000000E+00   # smuon_L decays
+    1.00000000E+00    2    1000022    13
+DECAY   2000013     0.10000000E+00   # smuon_R decays
+    1.00000000E+00    2    1000022    13
+DECAY   1000015     0.00000000E+00   # stau_1 decays
+DECAY   2000015     0.00000000E+00   # stau_2 decays
+#
+#         PDG            Width
+DECAY   1000012     0.00000000E+00   # snu_elL decays
+DECAY   1000014     0.00000000E+00   # snu_muL decays
+DECAY   1000016     0.00000000E+00   # snu_tauL decays
+DECAY   1000006     0.00000000E+00   # stop1 decays
+DECAY   1000021     0.00000000E+00   # gluino decays
+DECAY   1000022     0.00000000E+00   # neutralino1 decays
+DECAY   1000023     0.00000000E+00   # neutralino2 decays
+DECAY   1000024     0.00000000E+00   # chargino1+ decays
+DECAY   1000025     0.00000000E+00   # neutralino3 decays
+DECAY   1000035     0.00000000E+00   # neutralino4 decays
+DECAY   1000037     0.00000000E+00   # chargino2+ decays
+"""
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    RandomizedParameters = cms.VPSet(),
+)
+
+# weighted average of matching efficiencies for the full scan
+# must equal the number entered in McM generator params
+mcm_eff = 0.498
+model = "TSlepSlep"
+process = "SlepSlep"
+
+def matchParams(mass):
+    if mass < 199: return 76,0.580
+    elif mass < 299: return 76,0.525
+    elif mass < 499: return 76,0.457
+    else: return 76,0.457
+
+# Number of events for mass point, in thousands
+nevt = 10
+
+diag = 40
+xmin, xmax, xstep = 100, 450, 25
+ymin, ymax, ystep = 0, 250, 10
+
+# -------------------------------
+#    Constructing grid
+
+mpoints = []
+for mx in range(xmin, xmax+1, xstep):
+  ylist = []
+  if mx > ymax + diag: 
+    ylist.extend(range(ymin, ymax+1, ystep))
+  else:
+    ylist.extend(range(ymin, mx-diag+1, ystep))
+  for my in ylist:
+    mpoints.append([mx,my,nevt])
+ 
+for point in mpoints:
+    m_slep, mlsp = point[0], point[1]
+    qcut, tru_eff = matchParams(m_slep)
+    wgt = point[2]*(mcm_eff/tru_eff)
+    
+    if mlsp==0: mlsp = 1
+    slhatable = baseSLHATable.replace('%MLSP%','%e' % mlsp)
+    slhatable = slhatable.replace('%MSLEP%','%e' % m_slep)
+
+    basePythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        JetMatchingParameters = cms.vstring(
+            'JetMatching:setMad = off',
+            'JetMatching:scheme = 1',
+            'JetMatching:merge = on',
+            'JetMatching:jetAlgorithm = 2',
+            'JetMatching:etaJetMax = 5.',
+            'JetMatching:coneRadius = 1.',
+            'JetMatching:slowJetPower = 1',
+            'JetMatching:qCut = %.0f' % qcut, #this is the actual merging scale
+            'JetMatching:nQmatch = 5', #4 corresponds to 4-flavour scheme (no matching of b-quarks), 5 for 5-flavour scheme
+            'JetMatching:nJetMax = 2', #number of partons in born matrix element for highest multiplicity
+            'JetMatching:doShowerKt = off', #off for MLM matching, turn on for shower-kT matching
+            '6:m0 = 172.5',
+            'Check:abortIfVeto = on',
+        ), 
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'JetMatchingParameters'
+        )
+    )
+
+    generator.RandomizedParameters.append(
+        cms.PSet(
+            ConfigWeight = cms.double(wgt),
+            GridpackPath =  cms.string('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.3/sus_sms/SMS-SlepSlep/v2/SMS-TSlepSlep_mSlep-%i_tarball.tar.xz' % m_slep),
+            ConfigDescription = cms.string('%s_%i_%i' % (model, m_slep, mlsp)),
+            SLHATableForPythia8 = cms.string('%s' % slhatable),
+            PythiaParameters = basePythiaParameters,
+        ),
+    )
---
+++ GridpackWorkflow/production/models/TSlepSlep/TSlepSlep_grid.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+### Script to define scan grid
+
+### Authors:
+### Manuel Franco Sevilla
+### Ana Ovcharova
+
+import os,sys,math
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from grid_utils import *
+
+model = "TSlepSlep"
+process = "StauStau"
+
+# Number of events for mass point, in thousands
+nevt = 10
+
+diag = 40
+xmin, xmax, xstep = 100, 450, 25
+ymin, ymax, ystep = 0, 250, 10
+minDM = 40
+
+# -------------------------------
+#    Constructing grid
+
+
+mpoints = []
+for mx in range(xmin, xmax+1, xstep):
+  ylist = []
+  if mx > ymax + diag: 
+    ylist.extend(range(ymin, ymax+1, ystep))
+  else:
+    ylist.extend(range(ymin, mx-diag+1, ystep))
+  for my in ylist:
+    mpoints.append([mx,my,nevt])
+    
+## Test print out for repeated points
+mset = set()
+for mp in mpoints: mset.add(mp[0]*10000+mp[1])
+Npts, Ndiff = len(mpoints), len(mset)
+if Npts==Ndiff: print "\nGrid contains "+str(Npts)+" mass points. No duplicates\n"
+else: print "\n\nGRID CONTAINS "+str(Npts-Ndiff)+" DUPLICATE POINTS!!\n\n"
+
+# -------------------------------
+#     Plotting and printing
+
+Ntot = makePlot([mpoints], 'events', model, process, xmin, xmax, ymin, ymax)
+
+print '\nScan contains '+"{0:,.0f}".format(Ntot*1000)+" events"
+print 'Average matching efficiency (for McM and GEN fragment) = '+"{0:.3f}".format(getAveEff(mpoints,process))
+
+print
