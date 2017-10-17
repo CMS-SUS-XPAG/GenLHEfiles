bd5accd758f1e12f93121b5ee762ea307df521c0
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2017-10-17 03:03:08 -0700

    averaging time per event

--- GridpackWorkflow/test/scripts/harvestResults.py
+++ GridpackWorkflow/test/scripts/harvestResults.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
 import argparse
+import math
 
 argParser = argparse.ArgumentParser(description = "Argument parser")
 argParser.add_argument('--logDir', action='store', help="The script that should be run on condor")
+argParser.add_argument('--verbose', action="store_true", help="Show all the output")
 args = argParser.parse_args()
 
 
@@ -15,9 +17,13 @@ totalEvents = 0
 passedEvents = 0
 negWeightEvents = 0
 
+nResults = 0
+avgTime = 0
+
 for inFile in inputFiles:
-    print
-    print "Looking for results in file %s"%inFile
+    if args.verbose:
+        print
+        print "Looking for results in file %s"%inFile
     resultsLine = None
     line = ''
     with open(os.path.join(args.logDir,inFile),'r') as f:
@@ -26,19 +32,25 @@ for inFile in inputFiles:
             line = f.readline()
             if line.startswith("Total"):
                 resultsLine = line
+            if line.startswith(" - Avg event:"):
+                timeline = line
                 break
             if line == '': break
     if resultsLine:
-        print header
-        print resultsLine
+        if args.verbose:
+            print header
+            print resultsLine
         res = resultsLine.split()
         totalEvents += int(res[7])
         passedEvents += int(res[4])
         negWeightEvents += int(res[6])
+        avgTime += float(timeline.split()[3])
+        nResults += 1
     else:
-        print "No result found..."
+        if args.verbose:
+            print "No result found..."
 
-format_str = "{0:40}{1:10}"
+format_str = "{0:35}{1:>20}"
 
 print totalEvents
 print "### RESULTS ###"
@@ -46,5 +58,6 @@ print "Found %s files to scan."%len(inputFiles)
 print format_str.format("Total events:",totalEvents)
 print format_str.format("Passed events:",passedEvents)
 print format_str.format("Events w/ neg weight:",negWeightEvents)
-print format_str.format("Fraction passed:",round(float(passedEvents)/totalEvents,4))
+print format_str.format("Fraction passed:","%s +/- %s"%(round(float(passedEvents)/totalEvents,4), round(math.sqrt(float(passedEvents))/totalEvents,4)))
 print format_str.format("Out of which with neg weight:",round(float(negWeightEvents)/passedEvents,4))
+print format_str.format("Avg. Time:",round(float(avgTime)/nResults,1))
