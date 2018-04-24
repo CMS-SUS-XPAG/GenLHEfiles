5a4f05fcff17379dbe78433dde2b8db1cf11eb5a
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2018-04-24 00:33:41 -0700

    no need for additional patch

--- GridpackWorkflow/production/SMS-StopStop/writeallcards_SMS-StopStop.sh
+++ GridpackWorkflow/production/SMS-StopStop/writeallcards_SMS-StopStop.sh
@@ -6,7 +6,7 @@ PART="_mStop-"
 
 ### Create cards and SLHAs for all mass points
 
-for MPROD in {167..817..25} {183..833..25} {825..925..50} {1250..1400..50}; do
+for MPROD in {167..817..25} {183..833..25} {825..925..50} {1250..1400..50} 650 850 1200; do
     MODEL=${PROC}${PART}${MPROD}
     mkdir -p "${JOBS}/${MODEL}"
     cp ${TEMP}/${PROC}_run_card.dat "${JOBS}/${MODEL}/${MODEL}_run_card.dat"
