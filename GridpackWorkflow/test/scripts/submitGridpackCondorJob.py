c6021da736e9639c4c823405fd3988987f8a82ce
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2018-04-24 01:14:57 -0700

    pick up proxy automatically

--- GridpackWorkflow/test/scripts/submitGridpackCondorJob.py
+++ GridpackWorkflow/test/scripts/submitGridpackCondorJob.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser.add_argument('--cards-dir', dest='cardsDir', help="Path to directory with cards", required=True)
     parser.add_argument('--genproductions-dir', dest='genproductionsDir', help='Path to genproductions repository', default='/home/users/'+os.environ['USER']+'/mcProduction/genproductions')
     parser.add_argument('--no-sub', dest='noSub', action='store_true', help='Do not submit jobs')
-    parser.add_argument('--proxy', dest="proxy", help="Path to proxy", default='/tmp/x509up_u31156')
+    parser.add_argument('--proxy', dest="proxy", help="Path to proxy", default=os.environ["X509_USER_PROXY"])
     args = parser.parse_args()
 
     proc = args.proc
